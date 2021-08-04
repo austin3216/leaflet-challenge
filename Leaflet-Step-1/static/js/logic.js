9cb7ce3f0a4d7b63187eeadeb2e7c7d49d6061bf
austin3216 <aroyster@pm.me>
2021-08-04 16:05:44 -0600

    update logic.js

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -8,3 +8,34 @@ d3.json(usgs).then((data) => {
     createFeatures(data.features);
     console.log(data.features);
 });
+
+function createMap(earthquakes) {
+    // create the mapbox layers
+    var satellite = L.tilelayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+        maxZoom: 20,
+        id: "mapbox.satellite",
+        accessToken: API_KEY
+    });
+
+    var grayscale = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+        maxZoom: 20,
+        id: "mapbox.grayscale",
+        accessToken: API_KEY
+    });
+
+    var outdoors = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+        maxZoom: 20,
+        id: "mapbox.outdoors",
+        accessToken: API_KEY
+    });
+
+    var baseMap = {
+        "Satellite": satellite,
+        "Grayscale": grayscale,
+        "Outdoors": outdoors
+    };
+
+    var overlayMap = {
+        "Earthquakes": earthquakes
+    };
+}
